6f8afd6439bb9f2d463ccd6741cd8f06f7abb75d
missionbells <eriksigbladh@gmail.com>
2021-06-23 22:15:07 -0500

    init

---
+++ src/lib/commerce.js
@@ -0,0 +1,4 @@
+import Commerce from '@chec/commerce.js';
+
+export const commerce = new Commerce("pk_293144912b9c6ae3bdc4f46c61238298ab2aa151313a6", true);
+//export const commerce = new Commerce(process.env.REACT_APP_CHEC_PUBLIC_KEY, true);
